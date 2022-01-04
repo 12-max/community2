c6c0da95e002f8735903db0050b20c9151a545e4
12-max <1736047970@qq.com>
2022-01-04 20:48:54 +0800

    git add.

---
+++ src/main/java/zgz/community2/controller/IndexController.java
@@ -0,0 +1,15 @@
+package zgz.community2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String Index(){
+
+        return "index";
+    }
+
+}
