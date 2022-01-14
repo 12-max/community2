cce1365e963c876f1a36bda34068fbc7b713e53e
12-max <1736047970@qq.com>
2022-01-14 20:00:03 +0800

    修复功能

--- src/main/java/zgz/community2/controller/CommentController.java
+++ src/main/java/zgz/community2/controller/CommentController.java
@@ -5,8 +5,12 @@ import org.springframework.web.bind.annotation.*;
 import zgz.community2.dto.CommentDTO;
 import zgz.community2.dto.ResultDTO;
 import zgz.community2.mapper.CommentMapper;
+import zgz.community2.mapper.UserMapper;
 import zgz.community2.model.Comment;
+import zgz.community2.model.User;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 
 @RestController
@@ -15,6 +19,8 @@ public class CommentController {
     @Autowired
     private CommentMapper commentMapper;
 
+    @Autowired
+    private UserMapper userMapper;
 
 
     // 此方法含带参数 所以我们在浏览器执行 /test/comment 是执行的  一个
@@ -24,20 +30,27 @@ public class CommentController {
 //    这样的方法，所有一直重定向
     @PostMapping("/comment")
     @ResponseBody
-    public ResultDTO post(@RequestBody CommentDTO commentDTO){
+    public ResultDTO post(@RequestBody CommentDTO commentDTO,
+                          HttpServletRequest request){
+        User user = (User) request.getSession().getAttribute("user");
+
+
+        if (user==null){
+            return new ResultDTO(201,"用户未登录请先登录");
+        }
+        Integer id = userMapper.userName(user.getUsername()).getId();
         Comment comment = new Comment();
         comment.setParent_id(commentDTO.getParent_id());
         comment.setContent(commentDTO.getContent());
         comment.setType(commentDTO.getType());
         comment.setGmt_modified(System.currentTimeMillis());
         comment.setGmt_create(System.currentTimeMillis());
-        comment.setCommentator(1);
+        comment.setCommentator(id);
         comment.setLike_count(1L);
         commentMapper.inset(comment);
 
-        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
-        objectObjectHashMap.put("message","成功");
-        return new ResultDTO(0,"操作失败",null);
+
+        return new ResultDTO(200,"操作成功");
     }
 
 
--- src/main/java/zgz/community2/dto/ResultDTO.java
+++ src/main/java/zgz/community2/dto/ResultDTO.java
@@ -1,16 +1,46 @@
 package zgz.community2.dto;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ResultDTO {
 
     private Integer code;
     private String message;
-    private Object data;
+
+
+
+    public static ResultDTO error(Integer code,String message){
+        ResultDTO resultDTO = new ResultDTO(code,message);
+        return resultDTO;
+    }
+
+
+
+
+
+    public ResultDTO() {
+    }
+
+    public ResultDTO(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
 }
